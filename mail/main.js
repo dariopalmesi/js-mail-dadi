e19b9ed22d76824be915da3dce351eaaf6a78446
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-03 16:12:23 +0200

    upgrede mail commit

--- mail/main.js
+++ mail/main.js
@@ -2,21 +2,16 @@
 
 // punto 1
 // chiedere la email ad un utente random che si presta ad accedere ad un sito
-let names = ['Dario','Marta','Michele','Cristina','Luca','Francesco']
-console.log(names);
-
-let email = ['Dario@gmail.com','Michle@gmail.com','Marta@gmail.com','Cristina@gmail.com','Luca@gmail.com','FRancesco@gmail.com']
+let mail
+let email = ['dario@gmail.com','michle@gmail.com','marta@gmail.com','cristina@gmail.com','luca@gmail.com','francesco@gmail.com']
 console.log(email);
 
 
 // punto 2
 // verificare se fa parte di quel sito e di conseguenza stampare l'esito del controllo
-names = prompt('inserire nome')
-email = prompt('inserire email')
-
-if (names == email) {
-    alert('questa email è corretta')
-}else {
-    console.log('questa email non è presente ');
+mail = prompt('inserire nome')
+ 
+for (let i = 0; i = email.length; i++) {
+    console.log(email[i]);
     
 }
