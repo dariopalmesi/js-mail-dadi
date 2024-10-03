b55bdcb5d32a909f506c9ab390750185220dd9f8
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-03 16:25:36 +0200

    upgrade crash mail commit

--- mail/main.js
+++ mail/main.js
@@ -3,15 +3,21 @@
 // punto 1
 // chiedere la email ad un utente random che si presta ad accedere ad un sito
 let mail
-let email = ['dario@gmail.com','michle@gmail.com','marta@gmail.com','cristina@gmail.com','luca@gmail.com','francesco@gmail.com']
+let email = ['dario@gmail.com', 'michle@gmail.com', 'marta@gmail.com', 'cristina@gmail.com', 'luca@gmail.com', 'francesco@gmail.com']
 console.log(email);
 
 
 // punto 2
 // verificare se fa parte di quel sito e di conseguenza stampare l'esito del controllo
-mail = prompt('inserire nome')
- 
+mail = prompt('inserire mail')
+
 for (let i = 0; i = email.length; i++) {
     console.log(email[i]);
-    
+    if (mail == email) {
+        console.log('questa email è corretta');
+
+
+    } else {
+        console.log('questa email on è corretta');
+    }
 }
