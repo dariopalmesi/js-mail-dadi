80efa80eb4ae9aaf0a262cb6c49fc43bc689fa62
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-03 17:35:48 +0200

    upgrade gioco dei dati commit

--- gioco dei dadi/main.js
+++ gioco dei dadi/main.js
@@ -1,1 +1,11 @@
 console.log('ciao');
+
+// dati dei giocatori
+let giocatore = [1,2,3,4,5,6];
+let computer = [1,2,3,4,5,6];
+
+// confronto del risultato che determina il vincitore
+giocatore = Number(window.prompt("inserire numero"));
+computer = Number(window.prompt('inserire numero'))
+
+console.log(JSON.stringify(giocatore) === JSON.stringify(computer));
